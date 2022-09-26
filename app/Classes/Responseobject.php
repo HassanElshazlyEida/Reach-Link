4288a86a6e4c521b5e4fa013d06b7d67ad397d99
Hassan Elshazly Eida <hassanaboeata@gmail.com>
2022-09-26 03:01:52 +0200

    💥 NEW:  Response Trait

---
+++ app/Classes/Responseobject.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Classes;
+
+class Responseobject
+{
+    const status_ok = true;
+    const status_failed = false;
+    const code_ok = 200;
+    const code_failed = 400;
+    const code_unauthorized = 403;
+    const code_not_found = 404;
+    const code_error = 500;
+    public $status;
+    public $code;
+    public $msg = array();
+}
---
+++ app/Traits/GeneralApiTrait.php
@@ -0,0 +1,96 @@
+<?php
+namespace  App\Traits;
+
+use App\Classes\Responseobject;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
+
+trait GeneralTrait {
+
+    public function failed($validator)
+    {
+        $response   = new Responseobject();
+        $response->status = $response::status_failed;
+        $response->code = $response::code_failed;
+        foreach ($validator->errors()->getMessages() as $item) {
+            array_push($response->msg, $item);
+        }
+
+        return Response::json(
+            $response
+        );
+    }
+    public function returnError($msg){
+        return response()->json([
+            'status'=>false,
+            "msg"=>$msg
+        ]);
+    }
+    public function errorField($field){
+        $massage = __($field.' required');
+        return response()->json([
+            "msg" => $massage,
+            "status" => false
+        ]);
+    }
+    public function errorMessage($massage){
+        return response()->json([
+            "msg" => __($massage),
+            "status" => false
+        ]);
+    }
+    public function SuccessMessage($massage){
+        return response()->json([
+            "msg" => __($massage),
+            "status" => true
+        ]);
+    }
+    public function returnSuccess($msg=""){
+        return response()->json([
+            'status'=>true,
+            "msg"=>$msg
+        ]);
+    }
+    public function returnData($key,$value,$msg="",array $extra=[]){
+        return $this->data($key,$value,$msg="",true,$extra);
+    }
+    public function returnFailData($key,$value,$msg="",array $extra=[]){
+
+        return $this->data($key,$value,$msg="",false,$extra);
+    }
+    public function data($key,$value,$msg="",$status,array $extra=[]){
+
+        if (!empty($extra)) {
+            foreach ($extra as $key => $value) {
+                $data[$key] = $value;
+            }
+        }
+        $data = [
+            $key=>$value,
+            'status'=>$status,
+            "msg"=>$msg
+        ];
+        return response()->json($data);
+
+    }
+    public function ValidatorMessages($messages){
+        $data = [
+            'status'=>false,
+            "msg"=>'validation failed'
+        ];
+        if (!empty($messages)) {
+            $data["msg"] = array_values($messages)[0][0];
+        }
+        return response()->json($data);
+    }
+
+    public function returnValidationError($validator){
+        return $this->returnError($validator->errors()->first());
+    }
+
+    public function Validator($request, $rules, $niceNames = [])
+    {
+        return Validator::make($request->all(), $rules, [], $niceNames);
+    }
+
+}
