af5eff445f1912d36bae62f14a0401dbdc9b9a45
Hassan Elshazly Eida <hassanaboeata@gmail.com>
2022-09-27 05:28:34 +0200

    💥 NEW: Model & Migrations

---
+++ database/migrations/2022_09_27_031747_create_ads_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->longText("description");
+            $table->enum('type',['free','paid']);
+
+
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->foreign('category_id')
+            ->references('id')->on('categories')
+            ->nullOnDelete();
+
+            $table->unsignedBigInteger('advertiser_id');
+            $table->foreign('advertiser_id')
+            ->references('id')->on('users')
+            ->cascadeOnDelete();
+
+            $table->date("start_date");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ads');
+    }
+}
---
+++ database/migrations/2022_09_27_031758_create_tags_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+
+            $table->unsignedBigInteger('ad_id');
+            $table->foreign('ad_id')
+            ->references('id')->on('ads')
+            ->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
